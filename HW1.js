7bc33992053bd0aba4d1bebe2866994e1169c160
SwatiSingh-cli <69649352+SwatiSingh-cli@users.noreply.github.com>
2020-08-14 14:12:36 +0530

    Add files via upload

---
+++ HW1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Use JavaScript to Change Text</h2>
+<p>This example writes "Hello World!" into an HTML element with id="demo":</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "Hello World!";
+</script> 
+
+</body>
+</html>
